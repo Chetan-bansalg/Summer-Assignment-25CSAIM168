f086e9abb460b0496d93cfa4a0ba44f1b688f1c6
Chetan-bansalg <chetanbansal.real@gmail.com>
2026-07-09 19:08:37 +0530

    first code

---
+++ day 1/que1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+//Write a program to Calculate sum of first N natural numbers.
+int n;
+printf("enter the value of n natural no. to add ");
+scanf("%d",&n);
+
+return 0;
+
+
+
+
+}
