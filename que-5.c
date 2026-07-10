a5ae712db1f464183adf3e718e1379210b1353ad
Chetan-bansalg <chetanbansal.real@gmail.com>
2026-07-11 00:04:33 +0530

    que5

---
+++ que-5.c
@@ -0,0 +1,17 @@
+//Find sum of digits of a number
+#include <stdio.h>
+int main() {
+    int n, sum = 0;
+
+    printf("Enter Number: ");
+    scanf("%d", &n);
+
+    while(n!= 0) {
+        sum += n % 10;
+        n /= 10;
+    }
+
+    printf("Sum of Digits = %d", sum);
+
+    return 0;
+}
