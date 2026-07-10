dbaf4ef278aed09fc574f6b33b7848d736e94510
Chetan-bansalg <chetanbansal.real@gmail.com>
2026-07-10 23:47:57 +0530

    day 2

---
+++ day 1/que3.c
@@ -0,0 +1,15 @@
+//Find factorial of a number
+#include<stdio.h>
+int main(){
+int n,f=1;
+printf("enter the no. for factorial");
+scanf("%d",&n);
+for(int i=1;i<=n;i++)
+{
+f=f*i;
+
+}
+printf("%d",f);
+
+    return 0;
+}
---
+++ day 1/que4.c
@@ -0,0 +1,15 @@
+ digits in a number
+#include<stdio.h>
+int main(){
+int
+printf("enter the no. to check digits");
+scanf("%d",n);
+while(n!=0)
+{
+
+
+    
+}
+
+    return 0;
+}
